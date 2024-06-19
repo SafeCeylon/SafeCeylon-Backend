409de8f048f430880da81697e533950e2fa6b65d
Charitha Lakshan <mlakshancharitha@gmail.com>
2024-06-19 15:39:23 +0530

    Initial commit

---
+++ src/main/java/com/safeceylon/safeceylon/SafeCeylonApplication.java
@@ -0,0 +1,13 @@
+package com.safeceylon.safeceylon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeCeylonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeCeylonApplication.class, args);
+	}
+
+}
