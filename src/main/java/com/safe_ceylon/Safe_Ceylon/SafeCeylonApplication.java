c852189f60b5597145f61dfa25d42bf519e13b40
bimsara1999 <blankanatha@gmail.com>
2024-08-12 00:32:20 +0545

    Initial commit

---
+++ src/main/java/com/safe_ceylon/Safe_Ceylon/SafeCeylonApplication.java
@@ -0,0 +1,13 @@
+package com.safe_ceylon.Safe_Ceylon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeCeylonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeCeylonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safe_ceylon/Safe_Ceylon/SafeCeylonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safe_ceylon.Safe_Ceylon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeCeylonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
