8f7e60471c58545bbc2c4ecc8382e249fafc8566
bimsara1999 <blankanatha@gmail.com>
2024-08-12 12:57:07 +0545

    create officer entity

---
+++ src/main/java/com/safe_ceylon/Safe_Ceylon/entity/Officer.java
@@ -0,0 +1,11 @@
+package com.safe_ceylon.Safe_Ceylon.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Officer {
+}
