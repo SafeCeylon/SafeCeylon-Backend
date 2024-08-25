6dcea5af6de313ee23e88754b85651bfeaccfadd
bimsara1999 <blankanatha@gmail.com>
2024-08-25 12:23:08 +0545

    Create MD officer entity

---
+++ src/main/java/com/safe_ceylon/Safe_Ceylon/entity/MDOfficer.java
@@ -0,0 +1,42 @@
+package com.safe_ceylon.Safe_Ceylon.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name="MDOfficer")
+public class MDOfficer {
+
+    @Id
+    private Long empNumber;
+    @Column(nullable = false , unique = true)
+    private String nicNumber;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String contactNumber;
+
+    @Column(nullable = false)
+    private String officerPhotoUrl;
+
+
+
+
+
+}
