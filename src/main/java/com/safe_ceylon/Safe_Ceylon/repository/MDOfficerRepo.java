ce743e8873b01eb942f9b7506780d96b103dd154
bimsara1999 <blankanatha@gmail.com>
2024-11-18 20:29:02 +0545

    Update MD officer

--- src/main/java/com/safe_ceylon/Safe_Ceylon/repository/MDOfficerRepo.java
+++ src/main/java/com/safe_ceylon/Safe_Ceylon/repository/MDOfficerRepo.java
@@ -1,6 +1,7 @@
 package com.safe_ceylon.Safe_Ceylon.repository;
 
+import com.safe_ceylon.Safe_Ceylon.entity.MDOfficer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MDOfficerRepo extends JpaRepository<empNumber, Long> {
+public interface MDOfficerRepo extends JpaRepository<MDOfficer, Long> {
 }
